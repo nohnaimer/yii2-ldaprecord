c402fbf8c2d9da5f9756c9c2dac3017537eb318b
Maksim Astapenko <actapenko@gmail.com>
2020-04-06 15:38:27 +0300

    Update LdapRecord. Update README.

--- README.md
+++ README.md
@@ -22,10 +22,34 @@ or add
 to the require section of your `composer.json` file.
 
 
-Usage
+Configuration
 -----
 
-Once the extension is installed, simply use it in your code by  :
-
 ```php
-<?= \nohnaimer\ldaprecord\AutoloadExample::widget(); ?>```
+'components' => [
+...
+        'ad' => [
+            'class' => nohnaimer\ldaprecord\LdapRecord::class,
+            // Mandatory Configuration Options
+            'hosts'            => ['192.168.1.1'],
+            'base_dn'          => 'dc=local,dc=com',
+            'username'         => 'cn=admin,dc=local,dc=com',
+            'password'         => 'password',
+            
+            // Optional Configuration Options
+            'port'             => 389,
+            'follow_referrals' => false,
+            'use_ssl'          => false,
+            'use_tls'          => false,
+            'version'          => 3,
+            'timeout'          => 5,
+            
+            // Custom LDAP Options
+            'options' => [
+                // See: http://php.net/ldap_set_option
+                LDAP_OPT_X_TLS_REQUIRE_CERT => LDAP_OPT_X_TLS_HARD
+            ]
+        ],
+...
+],
+```
--- src/LdapRecord.php
+++ src/LdapRecord.php
@@ -6,41 +6,62 @@ use yii\base\Component;
 use LdapRecord\Connection;
 use LdapRecord\DetailedError;
 use LdapRecord\Auth\BindException;
-use LdapRecord\Configuration\ConfigurationException;
 
 /**
  * Class LdapRecord
  * @package nohnaimer\ldaprecord
+ *
+ * @mixin Connection
  */
 class LdapRecord extends Component
 {
     /**
-     * @var array
-     */
-    public $settings;
-    /**
      * @var Connection
      */
-    private $_connection;
+    protected $connection;
 
     /**
      * {@inheritdoc}
      */
-    public function init()
+    public function __construct($config = [])
     {
-        parent::init();
-
-        if (is_array($this->settings)) {
-            $this->_connection = new Connection($this->settings);
-            try {
-                $this->_connection->connect();
-            } catch (BindException $e) {
-                /** @var DetailedError $error */
-                $error = $e->getDetailedError();
-                throw new BindException("Code: {$error->getErrorCode()}, message: {$error->getErrorMessage()}, diagnostic: {$error->getDiagnosticMessage()}");
-            }
+        $this->connection = new Connection($config);
+        try {
+            $this->connection->connect();
+        } catch (BindException $e) {
+            /** @var DetailedError $error */
+            $error = $e->getDetailedError();
+            throw new BindException("Code: {$error->getErrorCode()}, message: {$error->getErrorMessage()}, diagnostic: {$error->getDiagnosticMessage()}");
         }
 
-        throw new ConfigurationException('Configuration options could not be found.');
+        parent::__construct();
+    }
+
+    /**
+     * @param string $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        return $this->connection->$name;
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $value
+     */
+    public function __set($name, $value)
+    {
+        $this->connection->$name = $value;
+    }
+
+    /**
+     * @param string $name
+     * @param array $params
+     * @return mixed
+     */
+    public function __call($name, $params)
+    {
+        return call_user_func_array([$this->connection, $name], $params);
     }
 }
